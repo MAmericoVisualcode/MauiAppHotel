65f1247ea9e1374645a5fba91cc3978e3285fab9
Marcelo Américo de Oliveira <marceloamericodeoliveira@outlook.com>
2024-11-17 13:47:14 -0300

    ADD recursos ao app

--- App.xaml.cs
+++ App.xaml.cs
@@ -5,8 +5,14 @@
         public App()
         {
             InitializeComponent();
-
-            MainPage = new AppShell();
+            MainPage = new NavigationPage(new Views.ContratacaoHospedagem());
+        }
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            var window = base.CreateWindow(activationState);
+            window.Width = 400;
+            window.Height = 600;
+            return window;
         }
     }
 }
---
+++ Views/SobreDesenvolvedor.xaml.cs
@@ -0,0 +1,9 @@
+namespace MauiAppHotel.View;
+
+public partial class SobreDesenvolvedor : ContentPage
+{
+	public SobreDesenvolvedor()
+	{
+		InitializeComponent();
+	}
+}
